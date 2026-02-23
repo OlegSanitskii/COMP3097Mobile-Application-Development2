a9c40a24b0e6a9eddcde1d4fffa0c31bdcd9d217
Ying Wu <wuying1203@gmail.com>
2026-02-23 16:13:48 -0500

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Ying Wu on 2026-02-23.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
