738eb9b983aa5f2be022fa3d4beafbd1c56632b8
Ying Wu <wuying1203@gmail.com>
2026-03-05 06:14:16 -0500

    Initial Commit

---
+++ SnapCal/SnapCalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapCalApp.swift
+//  SnapCal
+//
+//  Created by Ying Wu on 2026-03-05.
+//
+
+import SwiftUI
+
+@main
+struct SnapCalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
